1bebd060f992dea46e8a30619b3ed6e9461dc8e5
austin <lookcos@gmail.com>
2021-11-16 10:45:13 +0800

    queue

---
+++ 13. 队列/test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "queue.h"
+
+int main() {
+    char a = 'a';
+    char b = 'b';
+    char c = 'c';
+    
+    queue *queue = queue_create();
+    printf("%p\n", queue_pull_data(queue));
+    
+    queue_push_data(queue, &a);
+    queue_push_data(queue, &b);
+    queue_push_data(queue, &c);
+
+    while (queue->length)
+    {
+        printf("%c\n", *(char *)queue_pull_data(queue));
+    }
+    
+    queue_push_data(queue, &c);
+    queue_push_data(queue, &c);
+    
+    /* 释放队列中节点 */
+    queue_empty(queue);
+    printf("%p\n", queue_pull_data(queue));
+    
+    /* 释放队列 */
+    queue_release(queue);
+    
+    return 0;
+}
