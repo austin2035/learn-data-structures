c5c29faf9fd5c9e068f92f2146cb93aac728ad4d
austin <lookcos@gmail.com>
2021-11-16 10:43:01 +0800

    stack

---
+++ 12. 栈/test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "stack.h"
+
+int main()
+{   
+    char a = 'a';
+    char b = 'b';
+    char c = 'c';
+
+    /* 创建一个栈 */
+    stack *stack = stack_create();
+
+    printf("%p\n", stack_pop(stack));
+
+    /* 压栈 */
+    stack_push(stack, &a);
+    stack_push(stack, &b);
+    stack_push(stack, &c);
+
+    /* 出栈 */
+    while (stack->length > 0)
+    {
+        printf("%c\n", *(char *)stack_pop(stack));
+    }
+
+    /* 压栈 */
+    stack_push(stack, &a);
+    stack_empty(stack);
+    printf("%p\n", stack_pop(stack));
+    
+    /* 释放栈 */
+    stack_release(stack);
+    
+    return 0;
+}
